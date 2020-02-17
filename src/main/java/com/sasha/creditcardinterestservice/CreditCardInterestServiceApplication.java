f4450b691c71789db7ea5ad4d43ace439fbe26ab
Sasha Larson <sjlarson92@gmail.com>
2020-02-17 13:43:55 -0600

    initial commit

---
+++ src/main/java/com/sasha/creditcardinterestservice/CreditCardInterestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sasha.creditcardinterestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardInterestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardInterestServiceApplication.class, args);
+	}
+
+}

f912736eaf07bc431e65339e4c8303402934e61c
Sasha Larson <sjlarson92@gmail.com>
2020-02-19 22:07:21 -0600

    completed tests for test cases 2 and 3 and updated README

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # Credit Card Interest Service
 
+- Calculates credit card interest for a person. Returns total interest per person and wallet/credit card interest depending on api called
+
+## URL
+
+- /interest/customerAndCreditCard
+
+- /interest/customerAndWallet
+
+## Data Params
+
+```json
+[
+	{
+		"id": 1,
+		"firstName": "James", 
+		"lastName": "Bond",
+		"wallets": [
+			{
+				"id": 1,
+				"creditCards": [
+					{
+						"id": 1,
+						"type": "VISA",
+						"balance": 100
+					},
+					{
+						"id": 2,
+						"type": "MASTERCARD",
+						"balance": 100
+					},
+					{
+						"id": 3,
+						"type": "DISCOVER",
+						"balance": 100
+					}
+				]
+			}
+		]
+	}
+]
+```
+
+## Success Response:
+
+- /customerAndCreditCard
+- Code: 200
+- Content:
+```json
+[
+  {
+    "customerId": 1,
+    "firstName": "James",
+    "lastName": "Bond",
+    "totalInterest": 16.00,
+    "interestByType": {
+      "creditCard": {
+        "1": 10.00,
+        "2": 5.00,
+        "3": 1.00
+      }
+    }
+  }
+]
+```
+
+- /customerAndWallet
+- Code: 200
+- Content:
+```json
+[
+  {
+    "customerId": 1,
+    "firstName": "James",
+    "lastName": "Bond",
+    "totalInterest": 16.00,
+    "interestByType": {
+      "wallet": {
+        "1": 11.00,
+        "2": 5.00
+      }
+    }
+  }
+]
+```
+
+## Examples with Insomnia
+
+<img src= "/SashaCodingProjects/credit-card-interest-service/images/testcase1.png" width= 60% length= 60%>
+
+<img src= "/images/testCase2.png" width= 60% length= 60%>
+
+<img src= "/images/testCase3.png" width= 60% length= 60%>
--- src/test/java/com/sasha/creditcardinterestservice/controllers/InterestControllerTest.java
+++ src/test/java/com/sasha/creditcardinterestservice/controllers/InterestControllerTest.java
@@ -17,6 +17,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,7 +32,7 @@ public class InterestControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
-    public void getCustomerAndCreditCardInterest_when100CCBalanceAndVISACCType_returnCorrectResult() throws Exception {
+    public void getCustomerAndCreditCardInterest_when1Customer1WalletWith100CCBalance_returnCorrectResult() throws Exception {
 
         ArrayList<Customer> customers = new ArrayList<>();
         ArrayList<Wallet> wallets = new ArrayList<>();
@@ -52,7 +53,7 @@ public class InterestControllerTest {
                 100
         );
         ArrayList<CreditCard> creditCards = new ArrayList<>(Arrays.asList(creditCard1, creditCard2, creditCard3));
-        System.out.println("creditCards: " + creditCards.get(1));
+
         Wallet wallet = new Wallet(
                 1,
                 creditCards
@@ -94,4 +95,159 @@ public class InterestControllerTest {
                 .isEqualTo(BigDecimal.valueOf(1).setScale(2, RoundingMode.HALF_UP));
     }
 
+    @Test
+    public void getCustomerAndWalletInterest_when1Customer2WalletsWith100CCBalance_returnCorrectResult() throws Exception {
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        CreditCard creditCard1 = new CreditCard(
+                1,
+                CreditCard.Type.VISA,
+                100
+        );
+        CreditCard creditCard2 = new CreditCard(
+                2,
+                CreditCard.Type.MASTERCARD,
+                100
+        );
+        CreditCard creditCard3 = new CreditCard(
+                3,
+                CreditCard.Type.DISCOVER,
+                100
+        );
+        ArrayList<CreditCard> creditCards1 = new ArrayList<>(Arrays.asList(creditCard1, creditCard3));
+        ArrayList<CreditCard> creditCards2 = new ArrayList<>(Collections.singletonList(creditCard2));
+
+        Wallet wallet1 = new Wallet(
+                1,
+                creditCards1
+        );
+        Wallet wallet2 = new Wallet(
+                2,
+                creditCards2
+        );
+        ArrayList<Wallet> wallets = new ArrayList<>(Arrays.asList(wallet1, wallet2));
+        Customer customer = new Customer(
+                1,
+                "Austin",
+                "Powers",
+                wallets
+        );
+        customers.add(customer);
+
+        String customersContent = objectMapper.writeValueAsString(customers);
+        System.out.println("customersContent: " + customersContent);
+
+        MvcResult result = mockMvc.perform(
+                get("/interest/customerAndWallet")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(customersContent)
+                )
+                .andReturn();
+
+        String customerInterests = result.getResponse().getContentAsString();
+        System.out.println("customerInterests: " + customerInterests);
+        ArrayList<CustomerInterest> customerInterestArray = objectMapper
+                .readValue(customerInterests, new TypeReference<>() {});
+
+        assertThat(customerInterestArray.get(0).getTotalInterest())
+                .isEqualTo(BigDecimal.valueOf(16).setScale(2, RoundingMode.HALF_UP));
+
+        assertThat(customerInterestArray.get(0).getInterestByType().get("wallet").get(1))
+                .isEqualTo(BigDecimal.valueOf(11).setScale(2, RoundingMode.HALF_UP));
+
+        assertThat(customerInterestArray.get(0).getInterestByType().get("wallet").get(2))
+                .isEqualTo(BigDecimal.valueOf(5).setScale(2, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    public void getCustomerAndWalletInterest_when2Customers1WalletEachWith100CCBalance_returnCorrectResult() throws Exception {
+        CreditCard customer1creditCard1 = new CreditCard(
+                1,
+                CreditCard.Type.VISA,
+                100
+        );
+        CreditCard customer1creditCard2 = new CreditCard(
+                2,
+                CreditCard.Type.MASTERCARD,
+                100
+        );
+        CreditCard customer1creditCard3 = new CreditCard(
+                3,
+                CreditCard.Type.DISCOVER,
+                100
+        );
+        ArrayList<CreditCard> customer1creditCards = new ArrayList<>(Arrays
+                .asList(customer1creditCard1, customer1creditCard2, customer1creditCard3));
+
+        Wallet customer1Wallet = new Wallet(
+                1,
+                customer1creditCards
+        );
+
+        ArrayList<Wallet> customer1Wallets = new ArrayList<>(Collections
+                .singletonList(customer1Wallet));
+
+        Customer customer1 = new Customer(
+                1,
+                "Natasha",
+                "Romanoff",
+                customer1Wallets
+        );
+
+        CreditCard customer2creditCard1 = new CreditCard(
+                1,
+                CreditCard.Type.VISA,
+                100
+        );
+
+        CreditCard customer2creditCard2 = new CreditCard(
+                2,
+                CreditCard.Type.MASTERCARD,
+                100
+        );
+
+        ArrayList<CreditCard> customer2creditCards = new ArrayList<>(Arrays
+                .asList(customer2creditCard1, customer2creditCard2));
+
+        Wallet customer2Wallet = new Wallet(
+                1,
+                customer2creditCards
+        );
+
+        ArrayList<Wallet> customer2Wallets = new ArrayList<>(Collections
+                .singletonList(customer2Wallet));
+
+        Customer customer2 = new Customer(
+                2,
+                "Evelyn",
+                "Salt",
+                customer2Wallets
+        );
+        ArrayList<Customer> customers = new ArrayList<>(Arrays.asList(customer1, customer2));
+
+        String customersContent = objectMapper.writeValueAsString(customers);
+        System.out.println("customersContent: " + customersContent);
+
+        MvcResult result = mockMvc.perform(
+                get("/interest/customerAndWallet")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(customersContent)
+        )
+                .andReturn();
+
+        String customerInterests = result.getResponse().getContentAsString();
+        System.out.println("customerInterests: " + customerInterests);
+        ArrayList<CustomerInterest> customerInterestArray = objectMapper
+                .readValue(customerInterests, new TypeReference<>() {});
+
+        assertThat(customerInterestArray.get(0).getTotalInterest())
+                .isEqualTo(BigDecimal.valueOf(16).setScale(2, RoundingMode.HALF_UP));
+        assertThat(customerInterestArray.get(0).getInterestByType().get("wallet").get(1))
+                .isEqualTo(BigDecimal.valueOf(16).setScale(2, RoundingMode.HALF_UP));
+
+        assertThat(customerInterestArray.get(1).getTotalInterest())
+                .isEqualTo(BigDecimal.valueOf(15).setScale(2, RoundingMode.HALF_UP));
+        assertThat(customerInterestArray.get(1).getInterestByType().get("wallet").get(1))
+                .isEqualTo(BigDecimal.valueOf(15).setScale(2, RoundingMode.HALF_UP));
+    }
 }
